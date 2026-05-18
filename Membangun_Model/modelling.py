abf9733edf104dd94f0e7bc23fb157c16689a77f
Rizki Yanuar <ryanuar83@gmail.com>
2026-05-18 11:28:42 +0700

    update modelling.py

--- Membangun_Model/modelling.py
+++ Membangun_Model/modelling.py
@@ -1,12 +1,29 @@
 import pandas as pd
 import dagshub
 import mlflow
+from dotenv import load_dotenv
 from sklearn.ensemble  import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score
 
+load_dotenv()
+
 # Load dan splitting data
 def load_splitting_data(path):
+    """
+    Membaca dataset dari file CSV, memisahkan fitur dan target (Churn), 
+    serta melakukan proporsi pembagian data untuk pelatihan dan pengujian.
+
+    Args:
+        filepath (str): Jalur atau lokasi file dataset CSV (contoh: 'preprocessing/clean_data.csv').
+
+    Returns:
+        tuple: Mengembalikan 4 buah variabel data dalam bentuk tuple:
+            - X_train (DataFrame): Fitur yang digunakan untuk melatih mesin.
+            - X_test (DataFrame): Fitur yang digunakan untuk menguji mesin.
+            - y_train (Series): Kunci jawaban (target) untuk proses pelatihan.
+            - y_test (Series): Kunci jawaban (target) untuk proses pengujian.
+    """
     dataset = pd.read_csv(path)
 
     X = dataset.drop(columns=['Churn'])
@@ -22,19 +39,35 @@ def load_splitting_data(path):
     return X_train, X_test, y_train, y_test
 
 # Load Model
-def load_model(feature_train, feature_test, label_train, label_test):
-    dagshub.init(repo_owner='RizkiYanuar-Tech', repo_name='', mlflow=True)
+def load_train_model(feature_train, feature_test, label_train, label_test):
+    """
+    Melatih algoritma Random Forest Classifier dan secara otomatis mengirimkan 
+    laporan performa (metrik) beserta file model akhir ke server DagsHub melalui MLflow.
+
+    Args:
+        X_train (DataFrame): Data fitur pelatihan.
+        X_test (DataFrame): Data fitur pengujian.
+        y_train (Series): Target/label data pelatihan.
+        y_test (Series): Target/label data pengujian.
+
+    Returns:
+        None: Fungsi ini tidak mengembalikan nilai di dalam script, melainkan 
+              langsung menyimpan artefak dan metrik ke cloud (DagsHub).
+    """
+    dagshub.init(repo_owner='RizkiYanuar-Tech',
+                 repo_name='SMSML_Muhammad_Rizki_Yanuar',
+                 mlflow=True
+                )
 
     mlflow.set_experiment("Base Model RandomForest")
-    
-    with mlflow.start_run(run_name='')
+    with mlflow.start_run(run_name='RF_Baseline_Model'):
         rf = RandomForestClassifier(random_state=42, class_weight='balanced')
-        
+
         # Train
         rf.fit(feature_train, label_train)
         # Test
         y_pred = rf.predict(feature_test)
-        
+
         accuracy = accuracy_score(label_test, y_pred)
         precision = precision_score(label_test, y_pred)
         recall = recall_score(label_test, y_pred)
@@ -48,3 +81,7 @@ def load_model(feature_train, feature_test, label_train, label_test):
         mlflow.sklearn.log_model(rf, 'Random_Forest_Baseline')
 
         print('Model selesai dilatih dan disimpan dalam dagshub')
+
+if __name__ == '__main__':
+    X_train, X_test, y_train, y_test = load_splitting_data('clean_data.csv')
+    load_train_model(X_train, X_test, y_train, y_test)
